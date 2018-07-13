ae640742fbf2d7f7104fb67faf12aa3af7ac4206
GuoChang <827789895@qq.com>
2018-07-13 17:26:09 +0800

    优化request部分逻辑

--- company_spider_9.py
+++ company_spider_9.py
@@ -22,11 +22,6 @@ search_text_list = ['pump', 'fabric', 'glass', 'clothing', 'embroidery', 'E-Liqu
                     , 'furniture']
 search_text = search_text_list[-1]
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
-    'Connection': 'keep-alive'
-}
-
 search_type_dict = {'product': 'PRODUCT',
                     'supplier': 'SUPPLIER'}
 search_type = search_type_dict['supplier']
@@ -51,8 +46,38 @@ company_detail_product_dir_path = os.path.join(company_detail_dir_path, search_t
 
 first_html_path = os.path.join(company_list_pages_product_dir_path, search_text + r'_' + search_type + r'_0.html')
 
+headers_list = \
+    ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1',
+     'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0',
+     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+     'Opera/9.80 (Windows NT 6.1; U; zh-cn) Presto/2.9.168 Version/11.50',
+     'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 2.0.50727; SLCC2; ' +
+        '.NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; Tablet PC 2.0; .NET4.0E)',
+     'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; ' +
+        '.NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; InfoPath.3)',
+     'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; GTB7.0)',
+     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+     'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)',
+     'Mozilla/5.0 (Windows; U; Windows NT 6.1; ) AppleWebKit/534.12 (KHTML, like Gecko) Maxthon/3.0 Safari/534.12',
+     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; ' +
+        '.NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E)',
+     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; ' +
+        '.NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E; ' +
+        'SE 2.X MetaSr 1.0)',
+     'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.3 (KHTML, like Gecko) ' +
+        'Chrome/6.0.472.33 Safari/534.3 SE 2.X MetaSr 1.0',
+     'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; ' +
+        '.NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E)',
+     'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; ' +
+        '.NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E)',
+     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; ' +
+        '.NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E) ' +
+        'QQBrowser/6.9.11079.201',
+     'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+     'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0'
+     ]
 
-pool_size = 5
+pool_size = 10
 page_total = -1
 overwrite = False
 
@@ -68,7 +93,7 @@ if not os.path.exists(json_dir_path):
     os.mkdir(json_dir_path)
 
 
-def while_session_get(page_url, times=5000, sleep_time=0.3, new_sess=False):
+def while_session_get(page_url, times=5000, sleep_time=0.2, new_sess=False):
     """
     循环请求
     :return:
@@ -81,7 +106,7 @@ def while_session_get(page_url, times=5000, sleep_time=0.3, new_sess=False):
                 sess = requests.session()
 
             time.sleep(random.randint(5, 8) * sleep_time)
-            result = sess.get(page_url, headers=headers, timeout=5)
+            result = sess.get(page_url, headers=get_header(), timeout=5)
             # result = requests.get(page_url, headers=headers, proxies=proxies, timeout=5)
         except Exception as e:
             if while_times < times:
@@ -92,6 +117,17 @@ def while_session_get(page_url, times=5000, sleep_time=0.3, new_sess=False):
                 raise e
         else:
             return result
+
+
+def get_header():
+    """
+    获取header
+    :return:
+    """
+    return {
+            'User-Agent': random.choice(headers_list),
+            'Connection': 'keep-alive'
+            }
 
 
 def download_frist_html():

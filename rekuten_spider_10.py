526829c0163cdebb3a7311d0192f94e0c088c59a
GuoChang <827789895@qq.com>
2018-07-27 16:16:56 +0800

    采集关键字列表页、详情页

--- rekuten_spider_10.py
+++ rekuten_spider_10.py
@@ -13,6 +13,7 @@ import requests
 import os
 from lxml import etree
 import json
+import traceback
 import time
 import re
 
@@ -25,35 +26,38 @@ class RekutenSpiderSettings:
         if not isinstance(key_word_list, list):
             raise ValueError("must be list instance, eg:['リキッド', '電子タバコ', 'vape']")
 
-        self._set_request_setting_(key_word_list)
-        self._set_data_setting_()
+        self._set_request_setting(key_word_list)
+        self._set_data_setting()
 
-    def _set_request_setting_(self, key_word_list):
+    def _set_request_setting(self, key_word_list):
         """
         设置请求配置
         :return:
         """
         self.home_url = r'https://search.rakuten.co.jp/'
+        self.home_url_www = r'https://www.rakuten.co.jp/'
         # self.key_word_list = ['リキッド', '電子タバコ', 'vape']
         self.key_word_list = key_word_list
         self.key_word_str = '+'.join(self.key_word_list)
         # https://search.rakuten.co.jp/search/mall/リキッド+電子タバコ+vape/?p=5
         self.request_url = f'{self.home_url}search/mall/{self.key_word_str}/?p=1'
 
-    def _set_data_setting_(self):
+    def _set_data_setting(self):
         """
         设置数据配置
         :return:
         """
         self.home_path = r'E:\work_all\topease\takuten_spider_key'
         self.key_dir_path = os.path.join(self.home_path, self.key_word_str)
-        self.key_html_path = os.path.join(self.key_dir_path, 'html_dir')
-        self.key_json_path = os.path.join(self.key_dir_path, 'json_dir')
+        self.key_pages_html_dir_path = os.path.join(self.key_dir_path, 'pages_html_dir')
+        self.key_shop_info_html_dir_path = os.path.join(self.key_dir_path, 'shop_info_html_dir')
+        self.key_json_dir_path = os.path.join(self.key_dir_path, 'json_dir')
 
         self.make_dir(self.home_path)
         self.make_dir(self.key_dir_path)
-        self.make_dir(self.key_html_path)
-        self.make_dir(self.key_json_path)
+        self.make_dir(self.key_pages_html_dir_path)
+        self.make_dir(self.key_shop_info_html_dir_path)
+        self.make_dir(self.key_json_dir_path)
 
     @staticmethod
     def make_dir(dir_path):
@@ -109,24 +113,35 @@ class RekutenSpiderKey:
         self.settings = RekutenSpiderSettings(key_word_list)
         self.request = WhileRequests()
         self.html_page_number = -1
+        self.shop_name_url_json_path = None
 
     def run(self):
         """
         启动爬虫
         :return:
         """
-        self.gevent_download_page_html()
+        self.check_and_download_page_htmls()
+        self.gevent_parse_page_htmls()
+        self.check_and_download_shop_info_htmls()
 
-    def download_html(self, url, save_path):
+    def check_and_download_page_htmls(self):
+        """
+        检查并下载分页html页面
+        :return:
+        """
+        if len(os.listdir(self.settings.key_pages_html_dir_path)) < 150:
+            self.gevent_download_page_html()
+
+    def download_html(self, url, save_path, encoding='utf8'):
         """
         下载html页面
         :return:
         """
         if not os.path.exists(save_path):
             result = self.request.get(url)
-            # html_context = bytes(result.text, 'iso-8859-1').decode('utf-8')
-            html_content = result.text
-            with open(save_path, mode='w', encoding='utf8') as fp:
+
+            html_content = result.content.decode(encoding)
+            with open(save_path, mode='w', encoding=encoding) as fp:
                 fp.write(html_content)
             print(f'{url} ---- download OK')
         else:
@@ -138,8 +153,8 @@ class RekutenSpiderKey:
         :return:
         """
         try:
-            file_name = self.get_page_html_path_by_url(url)
-            file_path = os.path.join(self.settings.key_html_path, file_name)
+            file_name = self.get_page_html_file_name_by_url(url)
+            file_path = os.path.join(self.settings.key_pages_html_dir_path, file_name)
             self.download_html(url, file_path)
             if os.path.getsize(file_path) < 1024 * 5:
                 os.remove(file_path)
@@ -149,13 +164,13 @@ class RekutenSpiderKey:
             print(e)
             return url
 
-    def _parse_first_page_(self, first_page_url):
+    def _parse_first_page(self, first_page_url):
         """
         解析分页的首页，获取总页码数
         :return:
         """
-        file_name = self.get_page_html_path_by_url(first_page_url)
-        file_path = os.path.join(self.settings.key_html_path, file_name)
+        file_name = self.get_page_html_file_name_by_url(first_page_url)
+        file_path = os.path.join(self.settings.key_pages_html_dir_path, file_name)
         if not os.path.exists(file_path):
             self.download_html(first_page_url, file_path)
 
@@ -185,10 +200,10 @@ class RekutenSpiderKey:
         :return:
         """
         url = self.settings.request_url
-        self._parse_first_page_(url)
+        self._parse_first_page(url)
         return self.html_page_number
 
-    def get_page_html_path_by_url(self, url):
+    def get_page_html_file_name_by_url(self, url):
         """
         通过分页html的url起名字
         :param url:
@@ -206,17 +221,28 @@ class RekutenSpiderKey:
         else:
             raise ValueError("url no find '='")
 
+    def get_shop_info_html_file_name_by_url(self, url):
+        """
+        通过商店详情页的url起名字
+        :param url:
+        :return:
+        """
+        url_str = str(url)
+        url_shop_name = url_str.replace(self.settings.home_url_www, '').replace(r'/', '')
+        file_name = f'{url_shop_name}_info.html'
+        return file_name
+
     @staticmethod
-    def gevent_pool_requests(func, urls, gevent_pool_size=100):
+    def gevent_pool_requests(func, task_list, gevent_pool_size=100):
         """
         多协程请求
         :param func:
-        :param urls:
+        :param task_list:
         :param gevent_pool_size:
         :return:
         """
         gevent_pool = pool.Pool(gevent_pool_size)
-        result_list = gevent_pool.map(func, urls)
+        result_list = gevent_pool.map(func, task_list)
         return result_list
 
     def gevent_download_page_html(self):
@@ -230,14 +256,158 @@ class RekutenSpiderKey:
 
         page_url_list = [f'{url_pre}{i}' for i in range(1, page_number + 1)]
 
-        for i in range(50):
-            print(f'第{i+1}轮采集')
+        for i in range(5):
+            print(f'第{i+1}轮采集，本轮欲采集{len(page_url_list)}条')
             result_list = self.gevent_pool_requests(self.download_page_html, page_url_list)
             result_list = [j for j in result_list if j]
             if len(result_list) > 0:
                 page_url_list = result_list
             else:
                 break
+
+    def parse_page_html(self, file_path):
+        """
+        解析分页html文件
+        :param file_path:
+        :return:
+        """
+        content = self.read_file(file_path)
+
+        tree = etree.HTML(content)
+        product_shop_a_list = tree.xpath('//div[contains(@class, "dui-cards")]/div[contains(@class, "dui-card")]' +
+                                         '/div[contains(@class, "merchant")]/a')
+
+        shop_list = list()
+        for shop_item in product_shop_a_list:
+            shop_name_list = shop_item.xpath('./text()')
+            shop_url_list = shop_item.xpath('./@href')
+
+            if len(shop_name_list) == len(shop_url_list) > 0:
+                shop_name = shop_name_list[0]
+                shop_url = shop_url_list[0]
+                # 忽略广告跳转的url
+                if str(shop_url).find('?') > 0:
+                    continue
+                else:
+                    shop_list.append((shop_name, shop_url))
+        print(f'{file_path} parsed OK')
+        return shop_list
+
+    def gevent_parse_page_htmls(self):
+        """
+        多协程解析分页网页
+        :return:
+        """
+        shop_name_url_json_path = os.path.join(self.settings.key_json_dir_path, 'shop_name_url.json')
+        self.shop_name_url_json_path = shop_name_url_json_path
+
+        if not os.path.exists(shop_name_url_json_path):
+            dir_path = self.settings.key_pages_html_dir_path
+            file_path_list = [os.path.join(dir_path, file_name) for file_name in os.listdir(dir_path)]
+
+            result_list = self.gevent_pool_requests(self.parse_page_html, file_path_list)
+
+            # 分页list合并
+            shop_tuple_list = list()
+            for item_page_shop_list in result_list:
+                if item_page_shop_list:
+                    shop_tuple_list.extend(item_page_shop_list)
+
+            # 去重并生成list(<dict>)
+            shop_list = [{'shop_name': shop_name, 'shop_url': shop_url} for shop_name, shop_url in set(shop_tuple_list)]
+
+            self.save_to_json_file(shop_list, shop_name_url_json_path)
+        else:
+            print(f'{shop_name_url_json_path} is exists')
+
+    def check_and_download_shop_info_htmls(self):
+        """
+        检查并下载商店详情页
+        :return:
+        """
+        if os.path.exists(self.shop_name_url_json_path):
+            shop_name_url_json = self.read_json_file(self.shop_name_url_json_path)
+
+            if len(shop_name_url_json) != len(os.listdir(self.settings.key_shop_info_html_dir_path)):
+                self.gevent_download_shop_info_html(shop_name_url_json)
+            else:
+                print(f'all shop info htmls is exists')
+        else:
+            print(f'shop_name_url json file is no exists, download and parse pages htmls first')
+
+    def gevent_download_shop_info_html(self, shop_name_url_json):
+        """
+        多协程下载商店详情页
+        :param shop_name_url_json:
+        :return:
+        """
+        for i in range(5):
+            print(f'第{i+1}轮采集，本轮欲采集{len(shop_name_url_json)}条')
+            result_list = self.gevent_pool_requests(self.download_shop_info_html, shop_name_url_json)
+            result_list = [j for j in result_list if j]
+            if len(result_list) > 0:
+                shop_name_url_json = result_list
+            else:
+                break
+
+    def download_shop_info_html(self, item_shop_name_url_dict):
+        """
+        下载商店详情页
+        :param item_shop_name_url_dict:
+        :return:
+        """
+        try:
+            # shop_name = dict(item_shop_name_url_dict).get('shop_name', '')
+            shop_url = dict(item_shop_name_url_dict).get('shop_url', '')
+            shop_info_url = f'{shop_url}info.html'
+
+            file_name = self.get_shop_info_html_file_name_by_url(shop_url)
+            file_path = os.path.join(self.settings.key_shop_info_html_dir_path, file_name)
+
+            self.download_html(shop_info_url, file_path, encoding='EUC-JP')
+
+            if os.path.getsize(file_path) < 1024 * 5:
+                os.remove(file_path)
+                print(f'remove {file_path}')
+                return item_shop_name_url_dict
+        except Exception as e:
+            print(e)
+            print(traceback.format_exc())
+            return item_shop_name_url_dict
+
+    @staticmethod
+    def save_to_json_file(data, json_path):
+        """
+        持久化为json文件
+        :param data:
+        :param json_path:
+        :return:
+        """
+        with open(json_path, mode='w', encoding='utf8') as fp:
+            fp.write(json.dumps(data))
+
+    @staticmethod
+    def read_json_file(json_path):
+        """
+        读取json文件
+        :param json_path:
+        :return:
+        """
+        with open(json_path, mode='r', encoding='utf8') as fp:
+            json_file = json.load(fp)
+        return json_file
+
+    @staticmethod
+    def read_file(file_path, encoding='utf8'):
+        """
+        读取文件
+        :param file_path:
+        :param encoding:
+        :return:
+        """
+        with open(file_path, mode='r', encoding=encoding) as fp:
+            content = fp.read()
+        return content
 
 
 class Main:

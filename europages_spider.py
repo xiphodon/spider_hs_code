8cd8d92ff709b9cd5b101899dcc09341d7beb0b3
GuoChang <827789895@qq.com>
2021-04-23 17:51:41 +0800

    update

--- europages_spider.py
+++ europages_spider.py
@@ -5,6 +5,11 @@
 # @Site    : https://github.com/xiphodon
 # @File    : europages_spider.py
 # @Software: PyCharm
+from gevent import pool, monkey;
+
+monkey.patch_all()
+
+import hashlib
 import json
 import os
 from lxml import etree
@@ -31,6 +36,11 @@ class EuroPagesSpider(BaseSpider):
     # https://www.europages.co.uk/companies/results.html?ih=01570C
     activity_company_list_url = home_url + r'/companies/results.html'
     activity_company_list_pages_dir = os.path.join(home_dir, 'activity_company_list_pages_dir')
+
+    activity_company_list_json_dir = os.path.join(home_dir, 'activity_company_list_json_dir')
+    unique_company_list_json_path = os.path.join(home_dir, 'unique_company_list.json')
+
+    unique_company_pages_dir = os.path.join(home_dir, 'unique_company_pages_dir')
 
     def __init__(self):
         """
@@ -207,7 +217,8 @@ class EuroPagesSpider(BaseSpider):
                         with open(file_path, 'r', encoding='utf8') as fp:
                             content = fp.read()
                     else:
-                        result = self.requests.get(request_url, sleep_time=self.random_float(0, 0.01), request_times=200)
+                        result = self.requests.get(request_url, sleep_time=self.random_float(0, 0.01),
+                                                   request_times=200)
                         content = result.text
                         self.create_file(file_path, content)
 
@@ -222,6 +233,127 @@ class EuroPagesSpider(BaseSpider):
                         self.create_file(file_path, f'共{page_no}页')
                         break
 
+    def parse_activity_company_list_to_json(self):
+        """
+        解析业务公司列表
+        :return:
+        """
+        self.mkdir(self.activity_company_list_json_dir)
+
+        for item_dir_name in os.listdir(self.activity_company_list_pages_dir):
+            item_dir_path = os.path.join(self.activity_company_list_pages_dir, item_dir_name)
+            if not os.path.exists(item_dir_path):
+                continue
+            item_json_path = os.path.join(self.activity_company_list_json_dir, f'{item_dir_name}.json')
+            if os.path.exists(item_json_path):
+                continue
+            item_activity_company_list = list()
+            for item_file_name in os.listdir(item_dir_path):
+                if item_file_name.startswith("_"):
+                    continue
+                item_file_path = os.path.join(item_dir_path, item_file_name)
+                if not os.path.exists(item_file_path):
+                    continue
+
+                print(item_file_path)
+                # file_name = item_file_name[:-5]
+                # class_id, ih_id, page_no = file_name.split('_')
+
+                with open(item_file_path, 'r', encoding='utf8') as fp:
+                    content = fp.read()
+
+                selector = etree.HTML(content)
+                company_li_list = selector.xpath('//li[@class="article-company card card--1dp vcard"]')
+                for company_li in company_li_list:
+                    company_name = self.data_list_get_first(company_li.xpath('.//div[@class="company-info"]/a/@title'))
+                    company_href = self.data_list_get_first(company_li.xpath('.//div[@class="company-info"]/a/@href'))
+                    company_country = self.data_list_get_first(
+                        company_li.xpath('.//span[@class="company-country"]/text()'))
+                    company_city = self.data_list_get_first(
+                        company_li.xpath('.//span[@class="company-city"]/text()')).strip('-').strip()
+
+                    company_verified = 1 if len(company_li.xpath('.//div/img[@class="company-verified"]')) > 0 else 0
+                    # print(company_name, company_href, company_country, company_city, company_verified)
+                    item_activity_company_list.append({
+                        'c_name': company_name,
+                        'c_href': company_href,
+                        'c_country': company_country,
+                        'c_city': company_city,
+                        'c_verified': company_verified
+                    })
+                # break
+
+            with open(item_json_path, 'w', encoding='utf8') as fp:
+                json.dump(item_activity_company_list, fp)
+
+            # break
+
+    def merge_activity_company_to_unique_company_json(self):
+        """
+        合并按业务划分的公司数据，为唯一公司json数据
+        :return:
+        """
+        assert os.path.exists(self.activity_company_list_json_dir), 'activity_company_list_json_dir is not exists'
+
+        unique_company_dict = dict()
+        count = 0
+        for file_name in os.listdir(self.activity_company_list_json_dir):
+            file_name_prefix = file_name[:-5]
+            file_path = os.path.join(self.activity_company_list_json_dir, file_name)
+            print(file_path)
+            # class_id, ih_id = file_name_prefix.split('_')
+            if not os.path.exists(file_path):
+                continue
+
+            with open(file_path, 'r', encoding='utf8') as fp:
+                data_list = json.load(fp)
+
+            for data in data_list:
+                data: dict
+                c_key = hashlib.md5(data['c_href'].encode('utf8')).hexdigest()
+                unique_company_dict.setdefault(c_key, {
+                    'info': data,
+                    'activity': dict()
+                })
+                unique_company_dict[c_key]['activity'][file_name_prefix] = ''
+                count += 1
+            # break
+        print(f'共计{count}条，去重后共计{len(unique_company_dict)}条')
+        print('数据保存中...')
+        with open(self.unique_company_list_json_path, 'w', encoding='utf8') as fp:
+            json.dump(unique_company_dict, fp)
+        print('数据保存成功')
+
+    def gevent_pool_download_company_info_page(self, gevent_pool_size=10):
+        """
+        多协程下载公司详情页
+        :return:
+        """
+        self.mkdir(self.unique_company_pages_dir)
+        with open(self.unique_company_list_json_path, 'r', encoding='utf8') as fp:
+            data_dict = json.load(fp)
+        data_dict_len = len(data_dict)
+        gevent_pool = pool.Pool(gevent_pool_size)
+        result_list = gevent_pool.map(self.download_company_info_page,
+                                      [(i, data_dict_len, k, v) for i, (k, v) in enumerate(data_dict.items(), start=1)])
+        return result_list
+
+    def download_company_info_page(self, data):
+        """
+        下载公司详情页
+        :param data: 相关数据
+        :return:
+        """
+        i, size, c_key, content = data
+        print(f'\r{self.draw_data_progress(i, size)}', end='')
+        c_href = content['info']['c_href']
+        page_file_name = f'{c_key}.html'
+        page_file_path = os.path.join(self.unique_company_pages_dir, page_file_name)
+        if os.path.exists(page_file_path):
+            return
+        result = self.requests.get(c_href, sleep_time=self.random_float(0, 0.01), request_times=100)
+        self.create_file(page_file_path, result.text)
+
 
 if __name__ == '__main__':
     eps = EuroPagesSpider()
@@ -230,4 +362,8 @@ if __name__ == '__main__':
     # eps.download_item_class_pages()
     # eps.parse_class_pages()
     # eps.merge_business_directory_and_activity()
-    eps.download_activity_company_pages()
+    # eps.download_activity_company_pages()
+    # eps.parse_activity_company_list_to_json()
+    # eps.merge_activity_company_to_unique_company_json()
+    # 26,133,3321,6204755,1650470
+    eps.gevent_pool_download_company_info_page(gevent_pool_size=8)
